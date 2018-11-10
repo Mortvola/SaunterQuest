7927ecc0db1e0585824a8ea611969226aedd5231
richard <richard@sorenson.com>
2018-11-10 07:09:29 -0700

    Fixed issues with mile accumulation and computation of food weight.

--- calculate.php
+++ calculate.php
@@ -9,6 +9,22 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	// Include config file
 	require_once "config.php";
 	
+	function computeFoodWeight (&$day, $d, &$foodStart)
+	{
+//		echo "d = $d, foodStart = $foodStart\n";
+		$accum = 0;
+		for ($i = $d; $i >= $foodStart; $i--)
+		{
+//			echo $i, ": ";
+			$accum += $day[$i]["foodWeight"];
+//			echo $day[$i]["foodWeight"], ", ";
+			$day[$i]["accumWeight"] = $accum;
+//			echo $day[$i]["accumWeight"], "\n";
+		}
+		
+		$foodStart = $d + 1;
+	}
+	
 	$milesPerHour = 1; //1.5;
 	$startTime = 800;
 	$endTime = 1900;
@@ -25,6 +41,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	$segments = array(
 			array(0, array("start")),
 			array(5.0, array("muststop")),
+			array(36.6, array("arriveBefore")),
 			array(60.8, array("resupply", "muststop")),
 			array(110, array("resupply")),
 			array(210, array("linger")),
@@ -60,6 +77,11 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	$foodStart = $d;
 	$lingerHours = 0;
 	
+	$dayStartMile = 0;
+	$day[$d]["mile"] = $dayStartMile;
+	$day[$d]["foodWeight"] = $output[0]["weight"]; //todo: randomly select meal plan
+	$dayMiles = 0;
+	
 	for ($k = 0; $k < count($segments) - 1; $k++)
 	{
 		$segmentMiles = $segments[$k][0];
@@ -71,25 +93,35 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	 		$hoursPerDay = (($endTime - $startTime) - $midDayBreakDuration) / 100;
 	 		$hoursPerDay -= $lingerHours;
 	 		$lingerHours = 0;
-	 		$milesPerDay = $hoursPerDay * $milesPerHour;
+	 		$milesPerDay = $hoursPerDay * $milesPerHour - $dayMiles;
 	 		
-// 	 		echo "mile: $mile\n";
-// 	 		echo "segment mile: $segmentMiles\n";
-// 	 		echo "Miles/day = $milesPerDay\n";
+//  	 		echo "mile: $mile\n";
+//  	 		echo "day miles: $dayMiles\n";
+//  	 		echo "Miles/day = $milesPerDay\n";
 	 		
-	 		if ($segmentMiles + $milesPerDay >= $segments[$k + 1][0])
+ 	 		if ($segmentMiles + $milesPerDay >= $segments[$k + 1][0])
 	 		{
 	 			$deltaMiles = $segments[$k + 1][0] - $segmentMiles;
+	 			$dayMiles += $deltaMiles;
+	 			
+	 			if ($k == count($segments) - 2 || in_array("resupply", $segments[$k + 1][1]))
+	 			{
+	 				computeFoodWeight ($day, $d, $foodStart);
+	 			}
 	 			
 	 			if (in_array("muststop", $segments[$k + 1][1])
 				 || in_array("stop", $segments[$k + 1][1]))
 	 			{
-		 			//echo "Short day: $delta\n";
-		 			
-		 			$day[$d]["mile"] = $mile;
-		 			$day[$d]["foodWeight"] = $output[0]["weight"]; //todo: randomly select meal plan
-		 			//echo "i = $i, d = $d, mile = $mile\n";
-		 			$d++;
+	 				$d++;
+	 				if ($k < count($segments) - 2)
+	 				{
+		 				$day[$d]["mile"] = $day[$d - 1]["mile"] + $dayMiles;
+			 			$day[$d]["foodWeight"] = $output[0]["weight"]; //todo: randomly select meal plan
+			 			//echo "i = $i, d = $d, mile = $mile\n";
+			 			$dayMiles = 0;
+
+//			 			echo "*** Start of day $d: mile: ", $day[$d]["mile"], "\n";
+	 				}
 	 			}
 	 			else if (in_array("linger", $segments[$k + 1][1]))
 	 			{
@@ -107,51 +139,38 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	 				
 	 				if ($remainingHours <= 0)
 	 				{
-	 					$day[$d]["mile"] = $mile;
-	 					$day[$d]["foodWeight"] = $output[0]["weight"]; //todo: randomly select meal plan
-	 					//echo "i = $i, d = $d, mile = $mile\n";
 	 					$d++;
+	 					if ($k < count($segments) - 2)
+	 					{
+		 					$day[$d]["mile"] = $day[$d - 1]["mile"] + $dayMiles;
+		 					$day[$d]["foodWeight"] = $output[0]["weight"]; //todo: randomly select meal plan
+		 					//echo "i = $i, d = $d, mile = $mile\n";
+		 					$dayMiles = 0;
+//		 					echo "*** Start of day $d: mile: ", $day[$d]["mile"], "\n";
+	 					}
 	 					
 	 					$lingerHours =  -$remainingHours;
 	 				}
 	 			}
-	 			
-	 			$mile += $deltaMiles;
 	 		
 	 			break;
 	 		}
 	 		else 
 	 		{
-	 			$day[$d]["mile"] = $mile;
-	 			$day[$d]["foodWeight"] = $output[0]["weight"]; //todo: randomly select meal plan
-	 			//echo "i = $i, d = $d, mile = $mile\n";
-	 			$d++;
-	 			$mile += $milesPerDay;
+	 			$dayMiles += $milesPerDay;
 	 			$segmentMiles += $milesPerDay;
+	 			
+	 			$d++;
+	 			$day[$d]["mile"] = $day[$d - 1]["mile"] + $dayMiles;
+	 			$day[$d]["foodWeight"] = $output[0]["weight"]; //todo: randomly select meal plan
+	 			$dayMiles = 0;
+	 			
+//	 			echo "*** Start of day $d: mile: ", $day[$d]["mile"], "\n";
+	 			
+	 			//echo "i = $i, d = $d, mile = $mile\n";
 	 		}
 	 		
 	 		//echo "Miles = $mile\n";
-		}
-		
-		//echo "type at $k + 1:", $segment[$k + 1][0], "\n";
-		
-		// If this is the last segment or if this is a resupply point then
-		// compute the weight of the food for each day since the last resupply.
-		if ($k == count($segments) - 2 || in_array("resupply", $segments[$k + 1][1]))
-		{
-			$accum = 0;
-			for ($i = $d - 1; $i >= $foodStart; $i--)
-			{
-	//			echo $dayFoodWeight[$k][$i], ": ";
-				$accum += $day[$i]["foodWeight"];
-	//			echo $accum, ", ";
-				$day[$i]["accumWeight"] = $accum;
-	//			echo $dayPackWeight[$k][$i], "\n";
-			}
-			
-	//		ksort($day[$k]);
-	
-			$foodStart = $d;
 		}
 	}
 	
