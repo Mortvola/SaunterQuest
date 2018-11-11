85342e50f46c5da6ff12e9fb53c3f7a5be356fa8
richard <richard@sorenson.com>
2018-11-11 11:06:15 -0700

    Created strategy functions for ending later or starting earlier. Changed events into objects with enable/disable attributes. Added ability to disable an event if strategies to meet the requirements fail.

--- calculate.php
+++ calculate.php
@@ -52,11 +52,119 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 		$day[$d]["segment"] = $k;
 		$day[$d]["segmentMiles"] = $segmentMiles;
 		
-		echo "Setting Day $d, segment miles: $segmentMiles\n";
+		//echo "Initializing Day $d, segment: $k, segment miles: $segmentMiles\n";
 		
 		$dayMiles = 0;
 		$dayHours = 0;
 	}
+	
+	function StrategyEndLater ($timeShift, &$day, $d, &$hoursNeeded)
+	{
+		global $endTime;
+		
+		$earliestChangedDay = -1;
+		
+		// Try extending the end hours for the past days
+		for (; $d > 0; $d--)
+		{
+			$amountToShift = $endTime + $timeShift - $day[$d]["endTime"];
+			
+			if ($amountToShift > 0)
+			{
+				$day[$d]["endTime"] += $amountToShift;
+				$hoursNeeded -= $amountToShift;
+				
+				$day[$d]["notes"] = "changed end time to " . $day[$d]["endTime"] . ";";
+				//echo "Day $d, end time ", $day[$d]["endTime"], "\n";
+				
+				$earliestChangedDay = $d;
+				
+				if ($hoursNeeded <= 0)
+				{
+					break;
+				}
+			}
+			else
+			{
+				//echo "Day $d, could not move end time\n";
+			}
+
+			//
+			// If the start of this day can't be moved then look no earlier
+			//
+			if (array_key_exists ("cantMoveStartMiles", $day[$d]) && $day[$d]["cantMoveStartMiles"])
+			{
+				break;
+			}
+		}
+		
+		return $earliestChangedDay;
+	}
+	
+	
+	function StrategyStartEarlier ($timeShift, &$day, $d, &$hoursNeeded)
+	{
+		global $startTime;
+		
+		$earliestChangedDay = -1;
+		
+		//
+		// Try starting earlier for the past days
+		//
+		for (; $d > 0; $d--)
+		{
+			$amountToShift = $day[$d]["startTime"] - ($startTime - $timeShift);
+			
+			if ($amountToShift > 0)
+			{
+				$day[$d]["startTime"] -= $amountToShift;
+				$hoursNeeded -= $amountToShift;
+				
+				$day[$d]["notes"] = "changed start time to " . $day[$d]["startTime"] . ";";
+				//echo "Day $d, start time ", $day[$d]["startTime"], "\n";
+				
+				$earliestChangedDay = $d;
+				
+				if ($hoursNeeded <= 0)
+				{
+					break;
+				}
+			}
+			else
+			{
+				//echo "Day $d, could not move start time\n";
+			}
+			
+			//
+			// If the start of this day can't be moved then look no earlier
+			//
+			if (array_key_exists ("cantMoveStartMiles", $day[$d]) && $day[$d]["cantMoveStartMiles"])
+			{
+				break;
+			}
+		}
+		
+		return $earliestChangedDay;
+	}
+	
+	
+	function findEvent ($eventType, $events)
+	{
+		$foundEvent = null;
+		
+		for ($i = 0; $i < count($events); $i++)
+		{
+			if ($events[$i]->type == $eventType)
+			{
+				$foundEvent = $events[$i];
+				
+				break;
+			}
+		}
+		
+		return $foundEvent;
+	}
+	
 	
 	// Hiking profile
 	$milesPerHour = 1; //1.5;
@@ -67,14 +175,15 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	$totalMiles = 235;
 	
 	$segments = array(
-			array(0, array("start")),
-			array(5.0, array("muststop")),
-			array(36.6, array("arriveBefore")),
-			array(60.8, array("resupply", "muststop")),
-			array(110, array("resupply")),
-			array(178.3, array("arriveBefore")),
-			array(210, array("linger")),
-			array($totalMiles, array("stop")));
+			array("mile" => 0, "events" => array((object)[type => "start", enabled => true])),
+			array("mile" => 5.0, "events" => array((object)[type => "stop", enabled => true])),
+			array("mile" => 36.6, "events" => array((object)[type => "arriveBefore", enabled => true])),
+			array("mile" => 60.8, "events" => array((object)[type => "resupply", enabled => true], (object)[type => "stop", enabled => true])),
+			array("mile" => 110, "events" => array((object)[type => "resupply", enabled => true])),
+			array("mile" => 178.3, "events" => array((object)[type => "arriveBefore", enabled => true])),
+			array("mile" => 210, "events" => array((object)[type => "linger", enabled => true])),
+			array("mile" => $totalMiles, "events" => array((object)[type => "stop", enabled => true]))
+	);
 	
 	$noCamping = array(
 					array (18.1, 28.9));
@@ -117,7 +226,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	{
 		if (!$restart)
 		{
-			$segmentMiles = $segments[$k][0];
+			$segmentMiles = $segments[$k]["mile"];
 
 			if ($k == 0)
 			{
@@ -127,79 +236,94 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 		}
 		else
 		{
-			echo "Restarting at day $d\n";
+			//echo "Restarting at day $d\n";
 			
 			$segmentMiles = $day[$d]["segmentMiles"];
 			$dayMiles = 0;
+			$dayHours = 0;
 			$restart = false;
 		}
 		
 		for (;;)
 	 	{
+//	 		$z++;
+	 		
+	 		if ($z > 10)
+	 		{
+	 			echo "exited inner loop after $z iterations\n";
+	 			break;
+	 		}
+	 		
 	 		//echo "linger hours: $lingerHours\n";
 	 		
-	 		echo "Day $d, segment miles: " . $segmentMiles . "\n";
-	 		echo "Day $d, segment miles: " . $day[$d]["segmentMiles"] . "\n";
+	 		//echo "Day $d, segment miles: " . $segmentMiles . "\n";
+	 		//echo "Day $d, segment miles: " . $day[$d]["segmentMiles"] . "\n";
 	 		
 	 		$hoursPerDay = (($day[$d]["endTime"] - $day[$d]["startTime"]) - $midDayBreakDuration);
 	 		$hoursPerDay -= $lingerHours;
 	 		$lingerHours = 0;
 	 		$dayMilesRemaining = $hoursPerDay * $milesPerHour - $dayMiles;
 	 		
-	 		echo "Day $d, hours per day: $hoursPerDay\n";
+	 		//echo "Day $d, hours per day: $hoursPerDay\n";
 //  	 		echo "mile: $mile\n";
 //  	 		echo "day miles: $dayMiles\n";
 //  	 		echo "Miles/day = $dayMilesRemaining\n";
 	 		
-	 		if ($segmentMiles + $dayMilesRemaining >= $segments[$k + 1][0])
+	 		if ($segmentMiles + $dayMilesRemaining >= $segments[$k + 1]["mile"])
 	 		{
-	 			echo "Day $d, segment miles: $segmentMiles, next segment: " . $segments[$k + 1][0] . "\n";
+	 			//echo "Day $d, segment miles: $segmentMiles, next segment: " . $segments[$k + 1]["mile"] . "\n";
 	 			
-	 			$deltaMiles = $segments[$k + 1][0] - $segmentMiles;
+	 			$deltaMiles = $segments[$k + 1]["mile"] - $segmentMiles;
 	 			$dayMiles += $deltaMiles;
 	 			$hoursHiked = $deltaMiles / $milesPerHour;
 	 			$dayHours += $hoursHiked;
-	 			
+	 			$currentTime = $day[$d]["startTime"] + $dayHours;
 	 			$segmentMiles += $deltaMiles;
 	 			
-	 			if (in_array("arriveBefore", $segments[$k + 1][1]))
+	 			$event = findEvent("arriveBefore", $segments[$k + 1]["events"]);
+	 			
+	 			//if (in_array("arriveBefore", $segments[$k + 1]["events"]))
+	 			if ($event && $event->enabled)
 	 			{
 	 				$arriveBeforeTime = 12; //todo: this needs to come from the arriveBefore event
-	 				$currentTime = $startTime + $dayHours;
 	 				
-	 				echo "arrival restriction on day $d, currentTime = $currentTime\n";
+	 				//echo "arrival restriction on day $d, currentTime = $currentTime, requiredTime = $arriveBeforeTime\n";
 	 				
 	 				if ($currentTime > $arriveBeforeTime)
 	 				{
 	 					$hoursNeeded = $currentTime - $arriveBeforeTime;
-	 					
- 	 					echo "too late: ", $hoursHiked + $startTime, "\n";
-// 	 					echo "hours needed: ", $hoursNeeded, "\n";
 	 					
 	 					// extend the end time of each prior day an hour until the needed time is found and recompute mileage.
 	 					// todo: this needs to be a preference. The algorithm could add an hour to each
 	 					// prior day until the extra time needed is found or it could evenly divide the needed time
 	 					// across all days since the start, or a mix of the two.
 	 					
-	 					for ($d--; $d > 0; $d--)
-	 					{
-	 						if ($hoursNeeded > 1)
-	 						{
-		 						$day[$d]["endTime"] += 1;
-		 						$day[$d]["notes"] = "increased end time by 1 hour;";
-		 						$hoursNeeded -= 1;
-		 						
-		 						//echo "Day $d, end time ", $day[$d]["endTime"], "\n";
-	 						}
-	 						else
-	 						{
-	 							$day[$d]["endTime"] += $hoursNeeded;
-	 							$day[$d]["notes"] = "increased end time by " . $hoursNeeded . " hours;";
-	 							
-	 							break;
-	 						}
-	 					}
-	 					
+ 	 					$startDay1 = StrategyEndLater (1, $day, $d - 1, $hoursNeeded);
+ 	 					
+ 	 					if ($startDay1 == -1)
+ 	 					{
+ 	 						$startDay1 = $d;
+ 	 					}
+ 	 					
+ 	 					if ($hoursNeeded > 0)
+ 	 					{
+ 	 						$startDay2 = StrategyStartEarlier (1, $day, $d, $hoursNeeded);
+ 	 						
+ 	 						if ($startDay2 == -1)
+ 	 						{
+ 	 							$startDay2 = $d;
+ 	 						}
+ 	 						
+ 	 						$startDay1 = min($startDay1, $startDay2);
+ 	 					}
+
+ 	 					if ($hoursNeeded > 0)
+ 	 					{
+ 	 						$event->enabled = false;
+ 	 					}
+ 	 					
+ 	 					//$d = $startDay1;
+ 	 					
 	 					$k = $day[$d]["segment"] - 1; // decrease by one since the for loop will increase it by one
 	 					//echo "segment miles: $segmentMiles\n";
 	 					$restart = true;
@@ -212,18 +336,21 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	 				}
 	 			}
 	 			
-	 			if ($k == count($segments) - 2 || in_array("resupply", $segments[$k + 1][1]))
+	 			$event = findEvent("resupply", $segments[$k + 1]["events"]);
+	 			
+	 			if ($k == count($segments) - 2 || $event)
 	 			{
 	 				computeFoodWeight ($day, $d, $foodStart);
 	 				
-	 				if (in_array("resupply", $segments[$k + 1][1]))
+	 				if ($event)
 	 				{
 		 				$day[$d]["notes"] .= "resupply;";
 	 				}
 	 			}
 	 			
-	 			if (in_array("muststop", $segments[$k + 1][1])
-				 || in_array("stop", $segments[$k + 1][1]))
+	 			$event = findEvent("stop", $segments[$k + 1]["events"]);
+	 			
+	 			if ($event && $event->enabled)
 	 			{
 	 				// todo: error out if a mustStop is in a noCamping area?
 	 				
@@ -234,8 +361,13 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	 					$day[$d]["cantMoveStartMiles"] = true;
 	 				}
 	 			}
-	 			else if (in_array("linger", $segments[$k + 1][1]))
+	 				
+	 			$event = findEvent("linger", $segments[$k + 1]["events"]);
+	 			
+	 			if ($event && $event->enabled)
 	 			{
+	 				// todo: determien what it means to linger when already stopped.
+	 				
 	 				$lingerHours = 2; // todo: Linger hours need to come from the linger event.
 	 				
 	 				$remainingHours = ($hoursPerDay - $hoursHiked) - $lingerHours;
@@ -244,7 +376,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 //  	 				echo "linger: delta miles: $deltaMiles\n";
 //  	 				echo "linger: miles: $mile\n";
 //  	 				echo "linger: segment miles: $segmentMiles\n";
-//  	 				echo "linger: next segment miles: ", $segments[$k + 1][0], "\n";
+//  	 				echo "linger: next segment miles: ", $segments[$k + 1]["mile"], "\n";
 //  	 				echo "linger: remaining hours: $remainingHours\n";
 	 				
 	 				if ($remainingHours > 0)
@@ -281,7 +413,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	 				//echo "no camping start " . $noCamping[i][0] . " stop ". $noCamping
 	 				if (($segmentMiles + $dayMilesRemaining) > $noCamping[$i][0] && ($segmentMiles + $dayMilesRemaining) < $noCamping[$i][1])
 	 				{
-	 					echo "Day $d inside no camping area: " . ($segmentMiles + $dayMilesRemaining) . ": " . $noCamping[$i][0] . ", " . $noCamping[$i][1] . "\n";
+	 					//echo "Day $d inside no camping area: " . ($segmentMiles + $dayMilesRemaining) . ": " . $noCamping[$i][0] . ", " . $noCamping[$i][1] . "\n";
 	 					
 	 					//
 	 					// We are in a no camping area... need to move.
@@ -289,87 +421,28 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	 					$remainingMiles = $noCamping[$i][1] - ($segmentMiles + $dayMilesRemaining);
 	 					$hoursNeeded = $remainingMiles / $milesPerHour;
 	 					
-	 					echo "needed hours: $hoursNeeded\n";
+	 					//echo "needed hours: $hoursNeeded\n";
+
+	 					$startDay1 = StrategyEndLater (1, $day, $d, $hoursNeeded);
 	 					
-	 					// Try extending the end hours for the past days
-	 					for ($p = $d; $p > 0; $p--)
+	 					if ($startDay1 == -1)
 	 					{
-	 						if ($hoursNeeded > 1)
-	 						{
-	 							$day[$p]["endTime"] += 1;
-	 							$day[$p]["notes"] = "increased end time by 1 hour;";
-	 							$hoursNeeded -= 1;
-	 							
-	 							echo "Day $p, end time ", $day[$p]["endTime"], "\n";
-	 							
-	 							//
-	 							// If the start of this day can't be moved then look no further
-	 							//
-	 							if (array_key_exists ("cantMoveStartMiles", $day[$p]) && $day[$p]["cantMoveStartMiles"])
-	 							{
-	 								break;
-	 							}
-	 						}
-	 						else
-	 						{
-	 							$day[$p]["endTime"] += $hoursNeeded;
-	 							$day[$p]["notes"] = "increased end time by " . $hoursNeeded . " hours;";
-	 							$hoursNeeded = 0;
-	 							
-	 							echo "Day $p, end time ", $day[$p]["endTime"], "\n";
-	 							
-	 							break;
-	 						}
+	 						$startDay1 = $d;
 	 					}
-	 					
-	 					$startDay = $p;
 	 					
 	 					if ($hoursNeeded > 0)
 	 					{
-	 						//
-	 						// Try starting earlier for the past days
-	 						//
-	 						for ($p = $d; $p > 0; $p--)
+	 						$startDay2 = StrategyStartEarlier (1, $day, $d, $hoursNeeded);
+	 						
+	 						if ($startDay2 == -1)
 	 						{
-	 							if ($hoursNeeded > 1)
-	 							{
-	 								$day[$p]["startTime"] -= 1;
-	 								$day[$p]["notes"] = "decreased start time by 1 hour;";
-	 								$hoursNeeded -= 1;
-	 								
-	 								echo "Day $p, start time ", $day[$p]["startTime"], "\n";
-	 								
-	 								//
-	 								// If the start of this day can't be moved then look no further
-	 								//
-	 								if (array_key_exists ("cantMoveStartMiles", $day[$p]) && $day[$p]["cantMoveStartMiles"])
-	 								{
-	 									break;
-	 								}
-	 							}
-	 							else
-	 							{
-	 								$day[$p]["startTime"] -= $hoursNeeded;
-	 								$day[$p]["notes"] = "decreased end time by " . $hoursNeeded . " hours;";
-	 								$hoursNeeded = 0;
-	 								
-	 								echo "Day $p, start time ", $day[$p]["startTime"], "\n";
-	 								
-	 								break;
-	 							}
+	 							$startDay2 = $d;
 	 						}
+	 						
+	 						$startDay1 = min($startDay1, $startDay2);
 	 					}
-	 					
-	 					if ($startDay > $p)
-	 					{
-	 						$d = $p;
-	 					}
-	 					else
-	 					{
-	 						$d = $startDay;
-	 					}
-	 					
-	 					echo "Hours needed: $hoursNeeded\n";
+
+	 					$d = $startDay1;
 	 					
 	 					$k = $day[$d]["segment"] - 1; // decrease by one since the for loop will increase it by one
 	 					//echo "segment miles: $segmentMiles\n";
@@ -391,10 +464,9 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 	 				}
 	 			}
 
-	 			$z++;
-	 			
 	 			if ($z > 10)
 	 			{
+	 				echo "exited inner loop after $z iterations\n";
 	 				break;
 	 			}
 	 			
@@ -409,7 +481,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
  				$d++;
 	 			dayInitialize ($day, $d, $dayMiles, $dayHours, $k, $segmentMiles);
 	 			
-	 			echo "Day $d, segment miles: " . $day[$d]["segmentMiles"] . "\n";
+	 			//echo "Day $d, segment miles: " . $day[$d]["segmentMiles"] . "\n";
 //	 			echo "day $d start miles = " . $day[$d]["mile"] . "\n";
 	 		}
 	 		
@@ -418,6 +490,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 		
 		if ($z > 10)
 		{
+			echo "exited outer loop after $z iterations\n";
 			break;
 		}
 	}
