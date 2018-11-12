2ce3f6b35ceab4dc0de21f99f7eb4f171710afa9
richard <richard@sorenson.com>
2018-11-12 06:44:32 -0700

    Disabled StreetView and Fullscreen controls in google maps.

--- welcome.php
+++ welcome.php
@@ -67,6 +67,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 		    {
 	    	    center:new google.maps.LatLng(51.508742,-0.120850),
 	    	    zoom:5,
+	    	    streetViewControl:false,
+	    	    fullscreenControl:false,
 	    	};
 	    	
 	    	var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
