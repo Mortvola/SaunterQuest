615a1faebae330afff863bfef4619ccb422d3bef
richard <richard@sorenson.com>
2019-08-20 06:44:25 -0600

    Fixed bug where initial point was missing when starting a new segment when the previous segment reached the maximum number of points. Added ability to specify maximum number of points per segments and maximum distance between points.

--- exportTrail.php
+++ exportTrail.php
@@ -13,24 +13,38 @@ require_once "pointOfInterestUtils.php";
 
 $numberOfPoints = 0;
 $startDistance = 0;
-$minimumDistanceBetweenPoints = 400; // meters
+$maximumDistanceBetweenPoints = 400; // meters
 $maxNumberOfPointsPerSegment = 200;
 
 function writeTrackPoint ($xw, $point)
 {
 	global $numberOfPoints;
 	global $startDistance;
-	global $minimumDistanceBetweenPoints;
+	global $maximumDistanceBetweenPoints;
 	global $maxNumberOfPointsPerSegment;
+	global $prevPoint;
 
-	if ($point->dist - $startDistance >= $minimumDistanceBetweenPoints)
+	if ($point->dist - $startDistance >= $maximumDistanceBetweenPoints)
 	{
 		if ($numberOfPoints >= $maxNumberOfPointsPerSegment)
 		{
+			// End the current segment and start a new segment
+			// Use the last point of the prevous segment as the
+			// start of the next segment.
 			$xw->endElement (); // trkseg
 			$xw->startElement ("trkseg");
 
-			$numberOfPoints = 0;
+			if (isset ($prevPoint))
+			{
+				$xw->startElement ("trkpt");
+
+				$xw->writeAttribute ("lat", $prevPoint->lat);
+				$xw->writeAttribute ("lon", $prevPoint->lng);
+
+				$xw->endElement (); // trkpt
+
+				$numberOfPoints = 1;
+			}
 		}
 
 		$xw->startElement ("trkpt");
@@ -41,6 +55,8 @@ function writeTrackPoint ($xw, $point)
 		$xw->endElement (); // trkpt
 
 		$numberOfPoints++;
+
+		$prevPoint = $point;
 
 		$startDistance = $point->dist;
 	}
@@ -58,9 +74,9 @@ function writeWayPoint ($xw, $point, $name)
 
 	$xw->text ($name);
 
-	$xw->endElement ();
+	$xw->endElement (); // name
 
-	$xw->endElement (); // trkpt
+	$xw->endElement (); // wpt
 }
 
 
@@ -68,6 +84,16 @@ if ($_SERVER["REQUEST_METHOD"] == "GET")
 {
 	$userId = $_SESSION["userId"];
 	$userHikeId = $_GET["id"];
+
+	if (isset($_GET["segmentMax"]))
+	{
+		$maxNumberOfPointsPerSegment = $_GET["segmentMax"];
+	}
+
+	if (isset ($_GET["maxDistance"]))
+	{
+		$maximumDistanceBetweenPoints = $_GET("maxDistance");
+	}
 
 	$points = getRoutePointsFromUserHike ($userHikeId);
 
@@ -105,7 +131,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET")
 
 		$xw->startElement ("trkseg");
 
-		$startDistance = $points[0]->dist - $minimumDistanceBetweenPoints;
+		$startDistance = $points[0]->dist - $maximumDistanceBetweenPoints;
 
 		foreach ($points as $p)
 		{
