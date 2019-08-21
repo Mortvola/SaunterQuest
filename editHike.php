0a3f3cd44a76008bdc4c1a94ef3c807adccd0ecc
richard <richard@sorenson.com>
2019-08-21 06:47:58 -0600

    Added ability to specify the max number of points within a segment and the max distance between points. Applied coding standard PSR12 to exportTrail.php file.

--- editHike.php
+++ editHike.php
@@ -273,12 +273,40 @@ if ($hikeId)
 		</div>
 	</div> <!--  Modal -->
 
+	<!-- Modal -->
+	<div class="modal fade" id="exportTrail" role="dialog">
+		<div class="modal-dialog">
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 id=modalTitle class="modal-title">Export Trail</h4>
+				</div>
+				<div class="modal-body">
+					<form id='exportTrailForm'>
+						<label>Maximum number of points per segment:</label>
+						<input type="text" class='form-control' name='segmentMax' id='exportTrailSegmentMax' value='200'/>
+						<br/>
+						<label>Maximum distance between points:</label>
+						<input type="text" class='form-control' name='maxDistance' id='exportTrailMaxDistance' value='400'/>
+					</form>
+					<a id="exportTrailLink" download="export.gpx" hidden></a>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn" data-dismiss="modal">Cancel</button>
+					<button id="exportTrailButton" type="button" class="btn btn-default" data-dismiss="modal">Export</button>
+				</div>
+			</div>
+
+		</div>
+	</div> <!--  Modal -->
+
 	<div class="container-fluid" style="position:absolute;top:0;left:0;bottom:0;right:0;padding:0">
 		<nav id='sideBar' class='col-md-1' style="background-color:SlateGray;height:100%">
 		<ul class='list-unstyled'>
 		<li><a class="nav-link" href="/welcome.php" style="color:white">Home</a></li>
 		<li><a class="nav-link" href="/DayTemplates.php" style="color:white">Food Plans</a></li>
-		<li><a class="nav-link" href="/exportTrail.php?id=<?php echo $userHikeId ?>" style="color:white" download="export.gpx">Export Trail</a></li>
+		<li><a class="nav-link" href="javascript:showExportTrailModal()" style="color:white">Export Trail</a></li>
 		</ul>
 		</nav>
 		<div class="col-md-7" style="display:flex;flex-direction:column;padding:0;height:100%">
@@ -447,6 +475,24 @@ if ($hikeId)
 	<script>
 		<?php require_once "routeHighlighting.js"; ?>
 		<?php require_once "trailCondition.js"; ?>
+
+		function showExportTrailModal ()
+		{
+			$("#exportTrailButton").off('click');
+			$("#exportTrailButton").click(function () { exportTrail(); });
+
+			$("#exportTrail").modal ('show');
+		}
+
+		function exportTrail ()
+		{
+			var segmentMax = $("#exportTrailSegmentMax").val ();
+			var maxDistance = $("#exportTrailMaxDistance").val ();
+
+			$("#exportTrailLink").attr ("href", "/exportTrail.php?id=<?php echo $userHikeId ?>&segmentMax=" + segmentMax + "&maxDistance=" + maxDistance);
+			$("#exportTrailLink")[0].click ();
+		}
+
 	</script>
 
 	<script async defer
--- exportTrail.php
+++ exportTrail.php
@@ -16,138 +16,126 @@ $startDistance = 0;
 $maximumDistanceBetweenPoints = 400; // meters
 $maxNumberOfPointsPerSegment = 200;
 
-function writeTrackPoint ($xw, $point)
+function writeTrackPoint($xw, $point)
 {
-	global $numberOfPoints;
-	global $startDistance;
-	global $maximumDistanceBetweenPoints;
-	global $maxNumberOfPointsPerSegment;
-	global $prevPoint;
+    global $numberOfPoints;
+    global $startDistance;
+    global $maximumDistanceBetweenPoints;
+    global $maxNumberOfPointsPerSegment;
+    global $prevPoint;
 
-	if ($point->dist - $startDistance >= $maximumDistanceBetweenPoints)
-	{
-		if ($numberOfPoints >= $maxNumberOfPointsPerSegment)
-		{
-			// End the current segment and start a new segment
-			// Use the last point of the prevous segment as the
-			// start of the next segment.
-			$xw->endElement (); // trkseg
-			$xw->startElement ("trkseg");
+    if ($point->dist - $startDistance >= $maximumDistanceBetweenPoints) {
+        if ($numberOfPoints >= $maxNumberOfPointsPerSegment) {
+            // End the current segment and start a new segment
+            // Use the last point of the prevous segment as the
+            // start of the next segment.
+            $xw->endElement(); // trkseg
+            $xw->startElement("trkseg");
 
-			if (isset ($prevPoint))
-			{
-				$xw->startElement ("trkpt");
+            if (isset($prevPoint)) {
+                $xw->startElement("trkpt");
 
-				$xw->writeAttribute ("lat", $prevPoint->lat);
-				$xw->writeAttribute ("lon", $prevPoint->lng);
+                $xw->writeAttribute("lat", $prevPoint->lat);
+                $xw->writeAttribute("lon", $prevPoint->lng);
 
-				$xw->endElement (); // trkpt
+                $xw->endElement(); // trkpt
 
-				$numberOfPoints = 1;
-			}
-		}
+                $numberOfPoints = 1;
+            }
+        }
 
-		$xw->startElement ("trkpt");
+        $xw->startElement("trkpt");
 
-		$xw->writeAttribute ("lat", $point->lat);
-		$xw->writeAttribute ("lon", $point->lng);
+        $xw->writeAttribute("lat", $point->lat);
+        $xw->writeAttribute("lon", $point->lng);
 
-		$xw->endElement (); // trkpt
+        $xw->endElement(); // trkpt
 
-		$numberOfPoints++;
+        $numberOfPoints++;
 
-		$prevPoint = $point;
+        $prevPoint = $point;
 
-		$startDistance = $point->dist;
-	}
+        $startDistance = $point->dist;
+    }
 }
 
 
-function writeWayPoint ($xw, $point, $name)
+function writeWayPoint($xw, $point, $name)
 {
-	$xw->startElement ("wpt");
+    $xw->startElement("wpt");
 
-	$xw->writeAttribute ("lat", $point->lat);
-	$xw->writeAttribute ("lon", $point->lng);
+    $xw->writeAttribute("lat", $point->lat);
+    $xw->writeAttribute("lon", $point->lng);
 
-	$xw->startElement ("name");
+    $xw->startElement("name");
 
-	$xw->text ($name);
+    $xw->text($name);
 
-	$xw->endElement (); // name
+    $xw->endElement(); // name
 
-	$xw->endElement (); // wpt
+    $xw->endElement(); // wpt
 }
 
 
-if ($_SERVER["REQUEST_METHOD"] == "GET")
-{
-	$userId = $_SESSION["userId"];
-	$userHikeId = $_GET["id"];
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $userId = $_SESSION["userId"];
+    $userHikeId = $_GET["id"];
 
-	if (isset($_GET["segmentMax"]))
-	{
-		$maxNumberOfPointsPerSegment = $_GET["segmentMax"];
-	}
+    if (isset($_GET["segmentMax"])) {
+        $maxNumberOfPointsPerSegment = $_GET["segmentMax"];
+    }
 
-	if (isset ($_GET["maxDistance"]))
-	{
-		$maximumDistanceBetweenPoints = $_GET("maxDistance");
-	}
+    if (isset($_GET["maxDistance"])) {
+        $maximumDistanceBetweenPoints = $_GET["maxDistance"];
+    }
 
-	$points = getRoutePointsFromUserHike ($userHikeId);
+    $points = getRoutePointsFromUserHike($userHikeId);
 
-	if (isset ($points))
-	{
-		$xw = new XMLWriter ();
+    if (isset($points)) {
+        $xw = new XMLWriter();
 
-		$xw->openMemory ();
+        $xw->openMemory();
 
-		$xw->startDocument ("1.0");
+        $xw->startDocument("1.0");
 
-		$xw->setIndentString ("  ");
-		$xw->setIndent (true);
+        $xw->setIndentString("  ");
+        $xw->setIndent(true);
 
-		$xw->startElement ("gpx");
+        $xw->startElement("gpx");
 
-		$xw->writeAttribute ("xmlns:xsd", "http://www.w3.org/2001/XMLSchema");
-		$xw->writeAttribute ("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        $xw->writeAttribute("xmlns:xsd", "http://www.w3.org/2001/XMLSchema");
+        $xw->writeAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
 
-		$days = getSchedule ($userId, $points);
+        $days = getSchedule($userId, $points);
 
-		for ($d = 1; $d < count($days); $d++)
-		{
-			writeWayPoint ($xw, $days[$d], "Day " . $d . " camp");
-		}
+        for ($d = 1; $d < count($days); $d++) {
+            writeWayPoint($xw, $days[$d], "Day " . $d . " camp");
+        }
 
-		$pointsOfInterest = getPointsOfInterest ($userHikeId);
+        $pointsOfInterest = getPointsOfInterest($userHikeId);
 
-		foreach ($pointsOfInterest as $poi)
-		{
-			writeWayPoint ($xw, $poi, $poi->name);
-		}
+        foreach ($pointsOfInterest as $poi) {
+            writeWayPoint($xw, $poi, $poi->name);
+        }
 
-		$xw->startElement ("trk");
+        $xw->startElement("trk");
 
-		$xw->startElement ("trkseg");
+        $xw->startElement("trkseg");
 
-		$startDistance = $points[0]->dist - $maximumDistanceBetweenPoints;
+        $startDistance = $points[0]->dist - $maximumDistanceBetweenPoints;
 
-		foreach ($points as $p)
-		{
-			writeTrackPoint ($xw, $p);
-		}
+        foreach ($points as $p) {
+            writeTrackPoint($xw, $p);
+        }
 
-		$xw->endElement (); // trkseg
+        $xw->endElement(); // trkseg
 
-		$xw->endElement (); // trk
+        $xw->endElement(); // trk
 
-		$xw->endElement (); // gpx
+        $xw->endElement(); // gpx
 
-		$xw->endDocument ();
+        $xw->endDocument();
 
-		echo $xw->outputMemory ();
-	}
+        echo $xw->outputMemory();
+    }
 }
-
-?>
