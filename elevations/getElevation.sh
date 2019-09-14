afc3cab2473a2d1c46c35f838df56e6f9d9e191c
richard <richard@sorenson.com>
2019-09-14 14:43:06 -0600

    Updated getEleveation script with user and password switches.

--- elevations/getElevation.sh
+++ elevations/getElevation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/N33W108.SRTMGL1.hgt.zip
+wget --user=Mortvola --ask-password http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/${1}.SRTMGL1.hgt.zip
+
 
