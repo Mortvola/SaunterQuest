321524840a900afe3d5686c5d9f108ccf03cc46d
richard <richard@sorenson.com>
2019-09-22 21:17:04 -0600

    Changed to use postgres database. Added migrations for users and hike tables.

--- database/migrations/2019_09_23_030834_create_hike_table.php
+++ database/migrations/2019_09_23_030834_create_hike_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateHikeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('hike', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->bigInteger('userId');
+            $table->string('name');
         });
     }
 
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('hike');
     }
 }
