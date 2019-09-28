7c60abaeedbd57d2046be1c253bc84c177ee8b6b
richard <richard@backpackersplanner.com>
2019-09-28 21:44:04 +0000

    added migrations for node and edge tables. Modified getElevations script to unzip after the download.

---
+++ database/migrations/2019_09_28_212032_add_nav_edges.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNavEdges extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	DB::statement("CREATE SEQUENCE nav_edge_id_seq INCREMENT 1");
+
+	DB::statement(
+	    "CREATE TABLE nav_edges
+		(
+		  id integer NOT NULL DEFAULT nextval('nav_edge_id_seq'::regclass),
+		  start_node integer,
+		  end_node integer,
+		  start_fraction double precision,
+		  end_fraction double precision,
+		  forward_cost double precision,
+		  backward_cost double precision,
+		  line_id bigint,
+		  CONSTRAINT nav_edge_pkey PRIMARY KEY (id)
+		)");
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::drop('nav_edges');
+	    DB::statement('drop sequence nav_edge_id_seq');
+    }
+}
--- elevations/getElevation.sh
+++ elevations/getElevation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-wget --user=Mortvola --ask-password http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/${1}.SRTMGL1.hgt.zip
-
+FILE=${1}.SRTMGL1.hgt.zip
+wget --user=Mortvola --ask-password http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/${FILE}
+unzip ${FILE}
+rm ${FILE}
 
