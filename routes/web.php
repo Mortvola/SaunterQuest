8274d605a73f0ed27b966adfd0cc8db5df6570bd
richard <richard@sorenson.com>
2019-10-09 05:08:49 -0600

    Improved authentication checks.

--- routes/web.php
+++ routes/web.php
@@ -22,133 +22,140 @@ use App\Http\Controllers\ExportController;
 |
 */
 
-Route::view ('/', 'welcome');
+Route::view ('/', 'welcome')->name('welcome');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function () {
 
-Route::post('/hike', 'HikeController@post');
+    Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/hike/{hikeId}', function ($hikeId = null)
-{
-    return (new HikeController())->get($hikeId);
+    Route::get('/gear', function ()
+    {
+        return view('gear');
+    });
+
+    Route::post('/hike', 'HikeController@post');
+
+    Route::get('/hike/{hikeId}', function ($hikeId = null)
+    {
+        return (new HikeController())->get($hikeId);
+    });
+
+    Route::delete('/hike/{hikeId}', function ($hikeId)
+    {
+        return (new HikeController())->delete($hikeId);
+    });
+
+    Route::get('/hike/{hikeId}/route', function ($hikeId)
+    {
+        return (new RouteController())->get($hikeId);
+    });
+
+    Route::put('/hike/{hikeId}/route/startPoint', function ($hikeId, Request $request)
+    {
+        return (new RouteController())->setStartPoint($hikeId, $request);
+    });
+
+    Route::put('/hike/{hikeId}/route/endPoint', function ($hikeId, Request $request)
+    {
+        return (new RouteController())->setEndPoint($hikeId, $request);
+    });
+
+    Route::post('/hike/{hikeId}/route/waypoint', function ($hikeId, Request $request)
+    {
+        return (new RouteController())->addWaypoint($hikeId, $request);
+    });
+
+    Route::put('/hike/{hikeId}/route/waypoint/order', function ($hikeId, Request $request)
+    {
+        return (new RouteController())->updateWaypoints($hikeId, $request);
+    });
+
+    Route::put('/hike/{hikeId}/route/waypoint/{waypointId}/position', function ($hikeId, $waypointId, Request $request)
+    {
+        return (new RouteController())->updateWaypointPosition ($hikeId, $waypointId, $request);
+    });
+
+    Route::put('/hike/{hikeId}/route/waypoint/{waypointId}/details', function ($hikeId, $waypointId, Request $request)
+    {
+        return (new RouteController())->updateWaypointDetails($hikeId, $waypointId, $request);
+    });
+
+    Route::delete('/hike/{hikeId}/route/waypoint/{waypointId}', function ($hikeId, $waypointId)
+    {
+        return (new RouteController())->deleteWaypoint($hikeId, $waypointId);
+    });
+
+    Route::get('/hike/{hikeId}/schedule', function ($hikeId)
+    {
+        return (new ScheduleController())->get($hikeId);
+    });
+
+    // Point of Interest operations
+    Route::get('/hike/{hikeId}/pointOfInterest', function ($hikeId)
+    {
+        return (new PointOfInterestController())->get($hikeId);
+    });
+
+    Route::post('/hike/{hikeId}/pointOfInterest', function ($hikeId, Request $request)
+    {
+        return (new PointOfInterestController())->post($hikeId, $request);
+    });
+
+    Route::put('/hike/{hikeId}/pointOfInterest/{poiId}', function ($hikeId, $poiId, Request $request)
+    {
+        return (new PointOfInterestController())->put($hikeId, $poiId, $request);
+    });
+
+    Route::delete('/hike/{hikeId}/pointOfInterest/{poiId}', function ($hikeId, $poiId)
+    {
+        return (new PointOfInterestController())->delete($hikeId, $poiId);
+    });
+
+    Route::get('/hike/{hikeId}/resupplyLocation', function ($hikeId)
+    {
+        return (new ResupplyLocationController())->get($hikeId);
+    });
+
+    Route::get('/hike/{hikeId}/hikerProfile', function ($hikeId)
+    {
+        return (new HikerProfileController())->get($hikeId);
+    });
+
+    Route::post('/hike/{hikeId}/hikerProfile', function ($hikeId, Request $request)
+    {
+        return (new HikerProfileController())->post($hikeId, $request);
+    });
+
+    Route::put('/hike/{hikeId}/hikerProfile/{hikerProfileId}', function ($hikeId, $hikerProfileId, Request $request)
+    {
+        return (new HikerProfileController())->put($hikeId, $hikerProfileId, $request);
+    });
+
+    Route::delete('/hike/{hikeId}/hikerProfile/{hikerProfileId}', function ($hikeId, $hikerProfileId)
+    {
+        return (new HikerProfileController())->delete($hikeId, $hikerProfileId);
+    });
+
+    Route::get('/hike/{hikeId}/trailCondition', function ($hikeId)
+    {
+        return (new TrailConditionController())->get($hikeId);
+    });
+
+    Route::get('/hike/{hikeId}/resupplyPlan', function ($hikeId)
+    {
+        return (new ResupplyPlanController())->get($hikeId);
+    });
+
+    Route::get('/hike/{hikeId}/export', function ($hikeId, Request $request)
+    {
+        return (new ExportController())->get($hikeId, $request);
+    });
+
+    Route::get('/elevation', 'ElevationController@get');
+
+    Route::get('/map/intersections', 'MapController@getIntersections');
+    Route::get('/tileList', 'MapController@getTileList');
+    Route::get('/tile', 'TileController@get');
 });
-
-Route::delete('/hike/{hikeId}', function ($hikeId)
-{
-    return (new HikeController())->delete($hikeId);
-});
-
-Route::get('/hike/{hikeId}/route', function ($hikeId)
-{
-    return (new RouteController())->get($hikeId);
-});
-
-Route::put('/hike/{hikeId}/route/startPoint', function ($hikeId, Request $request)
-{
-    return (new RouteController())->setStartPoint($hikeId, $request);
-});
-
-Route::put('/hike/{hikeId}/route/endPoint', function ($hikeId, Request $request)
-{
-    return (new RouteController())->setEndPoint($hikeId, $request);
-});
-
-Route::post('/hike/{hikeId}/route/waypoint', function ($hikeId, Request $request)
-{
-    return (new RouteController())->addWaypoint($hikeId, $request);
-});
-
-Route::put('/hike/{hikeId}/route/waypoint/order', function ($hikeId, Request $request)
-{
-    return (new RouteController())->updateWaypoints($hikeId, $request);
-});
-
-Route::put('/hike/{hikeId}/route/waypoint/{waypointId}/position', function ($hikeId, $waypointId, Request $request)
-{
-    return (new RouteController())->updateWaypointPosition ($hikeId, $waypointId, $request);
-});
-
-Route::put('/hike/{hikeId}/route/waypoint/{waypointId}/details', function ($hikeId, $waypointId, Request $request)
-{
-    return (new RouteController())->updateWaypointDetails($hikeId, $waypointId, $request);
-});
-
-Route::delete('/hike/{hikeId}/route/waypoint/{waypointId}', function ($hikeId, $waypointId)
-{
-    return (new RouteController())->deleteWaypoint($hikeId, $waypointId);
-});
-
-Route::get('/hike/{hikeId}/schedule', function ($hikeId)
-{
-    return (new ScheduleController())->get($hikeId);
-});
-
-// Point of Interest operations
-Route::get('/hike/{hikeId}/pointOfInterest', function ($hikeId)
-{
-    return (new PointOfInterestController())->get($hikeId);
-});
-
-Route::post('/hike/{hikeId}/pointOfInterest', function ($hikeId, Request $request)
-{
-    return (new PointOfInterestController())->post($hikeId, $request);
-});
-
-Route::put('/hike/{hikeId}/pointOfInterest/{poiId}', function ($hikeId, $poiId, Request $request)
-{
-    return (new PointOfInterestController())->put($hikeId, $poiId, $request);
-});
-
-Route::delete('/hike/{hikeId}/pointOfInterest/{poiId}', function ($hikeId, $poiId)
-{
-    return (new PointOfInterestController())->delete($hikeId, $poiId);
-});
-
-Route::get('/hike/{hikeId}/resupplyLocation', function ($hikeId)
-{
-    return (new ResupplyLocationController())->get($hikeId);
-});
-
-Route::get('/hike/{hikeId}/hikerProfile', function ($hikeId)
-{
-    return (new HikerProfileController())->get($hikeId);
-});
-
-Route::post('/hike/{hikeId}/hikerProfile', function ($hikeId, Request $request)
-{
-    return (new HikerProfileController())->post($hikeId, $request);
-});
-
-Route::put('/hike/{hikeId}/hikerProfile/{hikerProfileId}', function ($hikeId, $hikerProfileId, Request $request)
-{
-    return (new HikerProfileController())->put($hikeId, $hikerProfileId, $request);
-});
-
-Route::delete('/hike/{hikeId}/hikerProfile/{hikerProfileId}', function ($hikeId, $hikerProfileId)
-{
-    return (new HikerProfileController())->delete($hikeId, $hikerProfileId);
-});
-
-Route::get('/hike/{hikeId}/trailCondition', function ($hikeId)
-{
-    return (new TrailConditionController())->get($hikeId);
-});
-
-Route::get('/hike/{hikeId}/resupplyPlan', function ($hikeId)
-{
-    return (new ResupplyPlanController())->get($hikeId);
-});
-
-Route::get('/hike/{hikeId}/export', function ($hikeId, Request $request)
-{
-    return (new ExportController())->get($hikeId, $request);
-});
-
-Route::get('/elevation', 'ElevationController@get');
-
-Route::get('/map/intersections', 'MapController@getIntersections');
-Route::get('/tileList', 'MapController@getTileList');
-Route::get('/tile', 'TileController@get');
-
