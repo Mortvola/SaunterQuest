3f2e2b0c5b332da8282229228b3a01cc95069b45
richard <richard@sorenson.com>
2019-10-10 05:05:59 -0600

    Changed source of elevation files.

--- elevations/getElevation.sh
+++ elevations/getElevation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-FILE=${1}.SRTMGL1.hgt.zip
-wget --user=Mortvola --ask-password http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/${FILE}
+#FILE=${1}.SRTMGL1.hgt.zip
+#wget --user=Mortvola --ask-password https://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/${FILE}
+FILE=${1}.hgt.zip
+wget https://dds.cr.usgs.gov/srtm/version2_1/SRTM1/Region_04/${FILE}
 unzip ${FILE}
 rm ${FILE}
 
