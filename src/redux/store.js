be15104d269e62148103acac9ccba31e3e2b9b67
richard <richard@sorenson.com>
2020-08-20 08:54:48 -0600

    Enable debugging based on NODE_ENV

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,20 +4,19 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import hikeApp from './reducers';
 
-const development = false;
 let enhancer = null;
 
-if (development) {
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(
+        thunkMiddleware, // lets us dispatch() functions
+    );
+}
+else {
     enhancer = composeWithDevTools(
         applyMiddleware(
             thunkMiddleware, // lets us dispatch() functions
             createLogger(), // neat middleware that logs actions
         ),
-    );
-}
-else {
-    enhancer = applyMiddleware(
-        thunkMiddleware, // lets us dispatch() functions
     );
 }
 
