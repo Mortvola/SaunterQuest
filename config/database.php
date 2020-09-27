4caab1b25c84200ffa654cbcf3f3aa853cc9bc4d
Richard Shields <richard@sorenson.com>
2020-09-27 09:27:19 -0600

    Added support for specifying multiple schema in .env

--- config/database.php
+++ config/database.php
@@ -74,6 +74,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'schema' => explode(',', env('DB_SCHEMA')),
             'sslmode' => 'disable',
         ],
 
