9571d3d46971a7c1daf9c2b16775482d08adf09b
mortvola <shields12345@msn.com>
2022-03-28 17:10:55 -0700

    Fixed webpack config to produce no css source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,102 +3,107 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const RemovePlugin = require('remove-files-webpack-plugin');
 
-const config = (folder, name, env) => ({
-  name,
-  mode: env.production ? 'production' : 'development',
-  devtool: env.production ? 'source-map' : 'inline-source-map',
-  entry: `${folder}/${name.charAt(0).toUpperCase()}${name.slice(1)}.tsx`,
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: `${name}.js`,
-    assetModuleFilename: 'images/[name][ext]',
-  },
-  module: {
-    rules: [
-      { test: /.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                exportLocalsConvention: 'camelCaseOnly',
-                localIdentName: '[name]-[local]-[hash:base64:5]',
+const config = (folder, name, env) => {
+  const values = {
+    name,
+    mode: env.production ? 'production' : 'development',
+    devtool: env.production ? undefined : 'inline-source-map',
+    entry: `${folder}/${name.charAt(0).toUpperCase()}${name.slice(1)}.tsx`,
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: `${name}.js`,
+      assetModuleFilename: 'images/[name][ext]',
+    },
+    module: {
+      rules: [
+        { test: /.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  exportLocalsConvention: 'camelCaseOnly',
+                  localIdentName: '[name]-[local]-[hash:base64:5]',
+                },
+                importLoaders: 1,
+                sourceMap: env.production === undefined,
               },
-              importLoaders: 1,
+            },
+          ],
+          include: /\.module\.css$/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+          exclude: /\.module\.css$/,
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.jsx?$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread'],
             },
           },
-        ],
-        include: /\.module\.css$/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
-        exclude: /\.module\.css$/,
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.jsx?$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          exclude: [/node_modules/, /public/],
+        },
+        {
+          test: /\.(vert|frag)$/,
+          use: {
+            loader: 'webpack-glsl-loader',
           },
         },
-        exclude: [/node_modules/, /public/],
-      },
-      {
-        test: /\.(vert|frag)$/,
-        use: {
-          loader: 'webpack-glsl-loader',
+      ],
+    },
+    plugins: [
+      new RemovePlugin({
+        before: {
+          test: [
+            {
+              folder: './public',
+              method: (file) => (name === 'app' ? /main\..*\.css/.test(file) : false),
+              recursive: false,
+            },
+            {
+              folder: './public',
+              method: (file) => (name === 'welcome' ? /welcome\..*\.css/.test(file) : false),
+              recursive: false,
+            },
+          ],
         },
-      },
+      }),
+      new MiniCssExtractPlugin({
+        filename: name === 'app'
+          ? 'main.[contenthash].css'
+          : 'welcome.[contenthash].css',
+      }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        publicPath: '/',
+        filename: `../resources/views/${name === 'app' ? 'home' : 'welcome'}-css.edge`,
+        templateContent: (param) => (
+          `<link rel="preload" href="${param.htmlWebpackPlugin.files.css}" as="style" onload="this.onload=null;this.rel='stylesheet'">`
+        ),
+      }),
     ],
-  },
-  plugins: [
-    new RemovePlugin({
-      before: {
-        test: [
-          {
-            folder: './public',
-            method: (file) => (name === 'app' ? /main\..*\.css/.test(file) : false),
-            recursive: false,
-          },
-          {
-            folder: './public',
-            method: (file) => (name === 'welcome' ? /welcome\..*\.css/.test(file) : false),
-            recursive: false,
-          },
-        ],
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: name === 'app'
-        ? 'main.[contenthash].css'
-        : 'welcome.[contenthash].css',
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      publicPath: '/',
-      filename: `../resources/views/${name === 'app' ? 'home' : 'welcome'}-css.edge`,
-      templateContent: (param) => (
-        `<link rel="preload" href="${param.htmlWebpackPlugin.files.css}" as="style" onload="this.onload=null;this.rel='stylesheet'">`
-      ),
-    }),
-  ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
-  },
-});
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    },
+  };
+
+  return values;
+};
 
 module.exports = [
   (env) => config('./client/Planner', 'app', env),
